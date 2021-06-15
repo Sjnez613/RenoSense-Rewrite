7f2fc39e9cc4762738f2f19c3c5f5e73d46533bb
Sjnez613 <52977333+Sjnez613@users.noreply.github.com>
2021-06-14 18:42:39 -0700

    Made y level customizable

--- src/main/java/me/alpha432/oyvey/features/modules/movement/AntiVoid.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/AntiVoid.java
@@ -1,11 +1,15 @@
 package me.alpha432.oyvey.features.modules.movement;
 
 import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.setting.Setting;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 
 public class AntiVoid
         extends Module {
+    public Setting<Float> yLevel = this.register(new Setting<Float>("YLevel", 0.5F, 0.1F, 10.0F));
+
+
     public AntiVoid() {
         super("AntiVoid", "Glitches you up from void.", Module.Category.MOVEMENT, false, false, false);
     }
@@ -15,7 +19,7 @@ public class AntiVoid
         if (AntiVoid.fullNullCheck()) {
             return;
         }
-        if (!AntiVoid.mc.player.noClip && AntiVoid.mc.player.posY <= 0.5) {
+        if (!AntiVoid.mc.player.noClip && AntiVoid.mc.player.posY <= yLevel.getValue()) {
             RayTraceResult trace = AntiVoid.mc.world.rayTraceBlocks(AntiVoid.mc.player.getPositionVector(), new Vec3d(AntiVoid.mc.player.posX, 0.0, AntiVoid.mc.player.posZ), false, false, false);
             if (trace != null && trace.typeOfHit == RayTraceResult.Type.BLOCK) {
                 return;
