ba5e21b677dcfb768c4069341076eedd2a24725d
Sjnez613 <52977333+Sjnez613@users.noreply.github.com>
2021-06-17 15:24:50 -0700

    Discord RPC Update

--- src/main/java/me/alpha432/oyvey/DiscordPresence.java
+++ src/main/java/me/alpha432/oyvey/DiscordPresence.java
@@ -30,7 +30,8 @@ public class DiscordPresence
         DiscordPresence.presence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
         DiscordPresence.presence.partyMax = 50;
         DiscordPresence.presence.partySize = 1;
-        DiscordPresence.presence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM=";
+        DiscordPresence.presence.spectateSecret = "c2puZXpzcGVjdDR0ZWxvbA==";
+        DiscordPresence.presence.joinSecret = "cmVub1MzbnNlam9pbg==";
         DiscordPresence.rpc.Discord_UpdatePresence(DiscordPresence.presence);
         (DiscordPresence.thread = new Thread(() -> {
             while (!Thread.currentThread().isInterrupted()) {
@@ -68,7 +69,11 @@ public class DiscordPresence
         }
         DiscordPresence.rpc.Discord_Shutdown();
     }
-    
+
+    public static void accept( ) {
+
+    }
+
     static {
         rpc = DiscordRPC.INSTANCE;
         DiscordPresence.presence = new DiscordRichPresence();
