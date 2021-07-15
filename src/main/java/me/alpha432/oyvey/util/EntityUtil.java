0b58174263d41c04af4bffd3e28bf56deebd18ad
Sjnez613 <52977333+Sjnez613@users.noreply.github.com>
2021-07-15 14:57:47 -0700

    Added jodah type tools and alpine event handler stuff

--- src/main/java/me/alpha432/oyvey/util/EntityUtil.java
+++ src/main/java/me/alpha432/oyvey/util/EntityUtil.java
@@ -649,5 +649,22 @@ public class EntityUtil
     public static boolean isAboveBlock(Entity entity, BlockPos blockPos) {
         return entity.posY >= (double) blockPos.getY();
     }
+
+    public static double[] calculateLookAt(final double px, final double py, final double pz, final EntityPlayer me) {
+        double dirx = me.posX - px;
+        double diry = me.posY - py;
+        double dirz = me.posZ - pz;
+        final double len = Math.sqrt(dirx * dirx + diry * diry + dirz * dirz);
+        dirx /= len;
+        diry /= len;
+        dirz /= len;
+        double pitch = Math.asin(diry);
+        double yaw = Math.atan2(dirz, dirx);
+        pitch = pitch * 180.0 / 3.141592653589793;
+        yaw = yaw * 180.0 / 3.141592653589793;
+        yaw += 90.0;
+        return new double[] { yaw, pitch };
+    }
+
 }
 
