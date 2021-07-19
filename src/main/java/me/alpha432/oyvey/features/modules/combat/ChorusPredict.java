7c4cd12f1ea194db27f03e4d549452e586939704
Sjnez613 <52977333+Sjnez613@users.noreply.github.com>
2021-07-19 11:23:32 -0700

    Made ChorusPredict more customizable

--- src/main/java/me/alpha432/oyvey/features/modules/combat/ChorusPredict.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/ChorusPredict.java
@@ -5,7 +5,9 @@ import me.alpha432.oyvey.event.events.PacketEvent;
 import me.alpha432.oyvey.event.events.Render3DEvent;
 import me.alpha432.oyvey.features.command.Command;
 import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.features.modules.client.ClickGui;
 import me.alpha432.oyvey.features.setting.Setting;
+import me.alpha432.oyvey.util.ColorUtil;
 import me.alpha432.oyvey.util.RenderUtil;
 import me.alpha432.oyvey.util.Timer;
 import net.minecraft.init.SoundEvents;
@@ -21,6 +23,13 @@ public class ChorusPredict extends Module {
     private BlockPos pos;
     private final Setting<Boolean> debug = this.register(new Setting("Debug", true));
     private final Setting<Integer> renderDelay = this.register(new Setting("RenderDelay", 4000, 0, 4000));
+    private Setting<Boolean> rainbow = register(new Setting("Rainbow", false));
+    private Setting<Integer> red = register(new Setting("Red", 0, 0, 255, v -> !this.rainbow.getValue()));
+    private Setting<Integer> green = register(new Setting("Green", 255, 0, 255, v -> !this.rainbow.getValue()));
+    private Setting<Integer> blue = register(new Setting("Blue", 0, 0, 255, v -> !this.rainbow.getValue()));
+    private Setting<Integer> alpha = register(new Setting("Alpha", 0, 0, 255, v -> !this.rainbow.getValue()));
+    private Setting<Integer> outlineAlpha = register(new Setting("OL-Alpha", 0, 0, 255, v -> !this.rainbow.getValue()));
+
 
     public ChorusPredict(){
         super("ChorusPredict", "Predicts Chorus", Category.COMBAT, true, false, false);
@@ -45,7 +54,7 @@ public class ChorusPredict extends Module {
                 pos = null;
                 return;
             }
-            RenderUtil.drawBoxESP(pos, new Color(0xC542FF), 1, true, true, 200);
+            RenderUtil.drawBoxESP(pos, rainbow.getValue() ? ColorUtil.rainbow(ClickGui.getInstance().rainbowHue.getValue()) : new Color(red.getValue(), green.getValue(), blue.getValue(), outlineAlpha.getValue()), 1.5F, true, true, alpha.getValue());
         }
     }
 
--- src/main/java/me/alpha432/oyvey/features/modules/player/NoTrace.java
+++
@@ -1,5 +0,0 @@
-package me.alpha432.oyvey.features.modules.player;
-
-public class NoTrace {
-
-}
